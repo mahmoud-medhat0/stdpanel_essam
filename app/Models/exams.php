4dee9c2dc1fe3fa7ec94b592f2f95b73a0755485
Mahmoud Medhat <hhetlar120@gmail.com>
2023-04-09 18:02:48 +0200

    fix exam model

--- app/Models/exams.php
+++ app/Models/exams.php
@@ -8,6 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class exams extends Model
 {
     use HasFactory;
-    protected $fillable = ['std_id','date','degree','created_at','branch_id','sec_type_id','exam_record'];
+    protected $fillable = ['std_id','date','degree','created_at','branch_id','sec_type_id','exam_record','payed'];
 
 }
